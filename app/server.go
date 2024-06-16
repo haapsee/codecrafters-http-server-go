41616ef41ce2300b3d0799fb36bd0df13f9810d6
Eemil Haapsaari <eemil@haapsaari.fi>
2024-06-16 20:58:59 +0300

    codecrafters submit [skip ci]

--- app/server.go
+++ app/server.go
@@ -15,9 +15,16 @@ func main() {
 		os.Exit(1)
 	}
 
-	_, err = l.Accept()
+	connection, err := l.Accept()
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
 	}
+
+	defer connection.Close()
+	handleConnection(connection)
+}
+
+func handleConnection(connection net.Conn) {
+    connection.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
 }
